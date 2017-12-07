509d902407db9f30b250ce66982ae76f2c193448
VladiG <vladig1984@gmail.com>
2017-12-07 18:54:04 +0200

    Upload Lab excercises

---
+++ DataTypesAndVariablesLab/CenturiesToMinutes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenturiesToMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            int years = 100 * centuries;
+            int days = (int)(365.2422 * years);
+            int hours = 24 * days;
+            int minutes = 60 * hours;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = " +
+                $"{minutes} minutes");
+
+
+
+        }
+    }
+}
